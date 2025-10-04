362f40ddeefbd8bcee43934be0c8e9eabf397c7a
Steve Gehrman <sgehrman@gmail.com>
2025-10-04 13:23:33 -0700

    sdf

--- tools/linux_build.sh
+++ tools/linux_build.sh
@@ -7,5 +7,8 @@ rm -rf build
 cmake -B build  
 cmake --build build --config Release -j $(nproc)
 
+rm -rf ../linux/libs/
+mkdir -p ../linux/libs/
+
 # you need -rP to preserve the symlinks
 cp -rP build/bin/*.so ../linux/libs/
